36400c3cff1649ae9d173939da70be561bdf2ac3
saurabh-solkar16 <saurabhsolkar4@gmail.com.com>
2018-11-02 02:29:26 +0530

    first commit

---
+++ main.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMDOT admin panel</title>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/animate.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  	#leftmenu{
+  		margin: 20px;
+  		 background-color: #357196;
+  		 height: auto;
+  		 width: auto;
+  		 color:white;
+  		  font-size:  20px;
+  		  color:white;
+  		font-family: "Comic Sans MS", cursive, sans-serif;
+  		  
+
+  	}
+
+  	.navbar-inverse{
+  		background-color: #304463;
+
+  	}
+  </style>
+</head>
+<body class="container-fluid" style="background-color: #D9DCD6;">
+<div class="row" style="margin-top: 10px;">
+	<div class="col-lg-2 animated bounceInDown " style=" padding-top:10px; " >
+<div>
+		<center>
+		<img src="images/oscar.png" style="height: 80px;width: 80px;border: 2px solid greay;padding: 2px;" class="img-circle">
+		<h4>Saurabh solkar</h4>
+
+		<span class="glyphicon glyphicon-cog"></span>
+	</center>
+</div>
+<div style="background-color: #357196;padding-top: 5px; padding-bottom: 5px;">
+	<center>
+		<div id="leftmenu" >Dashboard</div>
+		<div id="leftmenu">Orders</div>
+		<div id="leftmenu">Accaunt</div>
+		<div><center><span class="glyphicon "></span></center></div>
+	</center>
+	</div>	
+
+
+		</center>
+	</div>
+	
+	<div class="col-lg-10 animated bounceInRight " style="margin-top: 80px;">
+		
+<nav class="navbar navbar-inverse" >
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+	</div>
+
+</div>
+</body>
+</html>
